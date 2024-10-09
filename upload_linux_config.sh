9f7389686b965abfd993d03466fc1b130362d9e9
zubko <alexander.zubko@gmail.com>
2024-10-09 13:05:49 +0000

    update config

---
+++ upload_linux_config.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+current_dir=$(basename "$(pwd)")
+
+if [ "$current_dir" != "dotfiles" ]; then
+    echo "Error: Current directory is not 'dotfiles'"
+    echo "Current directory: $(pwd)"
+    exit 1
+fi
+
+echo "Getting the latest changes from Git repo..."
+
+git pull
+    
+echo "Copying dotfiles to this folder..."
+
+cp -r ~/.tmux.conf ./linux/
+
+echo "The new changes:"
+
+git add .
+git diff --staged
+
+echo "Upload the changes? (Y/n)"
+read -r confirm
+if [ "$confirm" != "y" ] && [ -n "$confirm" ]; then
+    exit 1
+fi
+
+git commit -m "update config"
+git push
+
+exit 0
+
